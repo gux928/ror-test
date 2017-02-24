8d06f1310ec3957ab49bedd51c4b61f5bedf7866
GuXiang <guxiang.yz@gmail.com>
2017-02-24 20:48:05 +0800

    Revert "Revert "Merge branch 'iamges' of git.coding.net:guxiang/zggf-ror into iamges""
    This reverts commit adb57f32609f932013e6d0f1448a6685727696dc.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ApplicationRecord
   belongs_to :imageable, polymorphic: true
 
   def show_png
-    folder = File.expand_path(".")+"/upload/"
+    folder = File.expand_path(".")+"/pic_tmp/"
     png_name = folder + self.file_name
     # send_file png_name, type: 'image/png', disposition: 'inline'
   end
